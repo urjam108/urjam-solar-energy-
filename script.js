0012ad7e2bbc78289c4cd9dab5ae51ad27082d7c
URJAM FOUNDATION <urjam.foundation@gmail.com>
2026-06-29 06:56:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function calculateSolar(){
 
+let bill=document.getElementById("bill").value;
+
+let system=document.getElementById("system").value;
+
+let saving=bill*0.90;
+
+let cost=system*65000;
+
+document.getElementById("saving").innerHTML=
+"Estimated Monthly Saving : ₹"+saving;
+
+document.getElementById("cost").innerHTML=
+"Estimated System Cost : ₹"+cost;
+
+}
