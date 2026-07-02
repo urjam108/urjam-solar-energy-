197a8b2262d32139ef2eb273e17febe357048df7
URJAM FOUNDATION <urjam.foundation@gmail.com>
2026-07-02 06:04:25 +0530

    Update script-v2.js

--- script-v2.js
+++ script-v2.js
@@ -1,1 +1,42 @@
+// ================================
+// URJAM SOLAR ENERGY V2
+// GLOBAL PREMIUM JS
+// ================================
 
+// Navbar Shadow on Scroll
+
+window.addEventListener("scroll",()=>{
+
+const header=document.querySelector(".header");
+
+if(window.scrollY>50){
+
+header.style.boxShadow="0 15px 40px rgba(0,0,0,.08)";
+
+}
+
+else{
+
+header.style.boxShadow="none";
+
+}
+
+});
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
